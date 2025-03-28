2a441cd72325010cb95aded8ce04b54ca2b9377c
NET_12_NguyenPhiHung_SE172907 <hungnpse172907@fpt.edu.vn>
2025-03-28 14:56:41 +0700

    Update Sung-Jin-Woo

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -19,42 +19,55 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (PlayerController.Instance.gameObject.activeSelf){
+        if (PlayerController.Instance.gameObject.activeSelf)
+        {
             // face the player
-            if (PlayerController.Instance.transform.position.x > transform.position.x){
+            if (PlayerController.Instance.transform.position.x > transform.position.x)
+            {
                 spriteRenderer.flipX = true;
-            } else {
+            }
+            else
+            {
                 spriteRenderer.flipX = false;
             }
             // push back
-            if (pushCounter > 0){
+            if (pushCounter > 0)
+            {
                 pushCounter -= Time.deltaTime;
-                if (moveSpeed > 0){
+                if (moveSpeed > 0)
+                {
                     moveSpeed = -moveSpeed;
                 }
-                if (pushCounter <= 0){
+                if (pushCounter <= 0)
+                {
                     moveSpeed = Mathf.Abs(moveSpeed);
                 }
             }
             // move towards the player
             direction = (PlayerController.Instance.transform.position - transform.position).normalized;
             rb.linearVelocity = new Vector2(direction.x * moveSpeed, direction.y * moveSpeed);
-        } else {
+        }
+        else
+        {
             rb.linearVelocity = Vector2.zero;
         }
     }
 
-    void OnCollisionStay2D(Collision2D collision){
-        if (collision.gameObject.CompareTag("Player")){
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
             PlayerController.Instance.TakeDamage(damage);
         }
     }
 
-    public void TakeDamage(float damage){
+    public void TakeDamage(float damage)
+    {
         health -= damage;
         DamageNumberController.Instance.CreateNumber(damage, transform.position);
         pushCounter = pushTime;
-        if (health <= 0){
+        if (health <= 0)
+        {
             Destroy(gameObject);
             Instantiate(destroyEffect, transform.position, transform.rotation);
             PlayerController.Instance.GetExperience(experienceToGive);
